7103d16808f714f1a747c61068362a4f939b8d64
Archil-Milorava <achimilorava16@gmail.com>
2024-11-15 20:46:34 +0400

    deploying

--- server.js
+++ server.js
@@ -9,23 +9,30 @@ import cookieParser from "cookie-parser";
 import { authenticatedUser } from "./middleware/authMiddleware.js";
 import userRouter from "./routes/userRoutes.js";
 import cloudinary from "cloudinary";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
 dotenv.config();
 
 /////////////////////////////
 const app = express();
 
-
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -34,8 +41,12 @@ cloudinary.config({
 });
 
 app.use("/api/v1/jobs", jobRouter);
-app.use("/api/v1/users",authenticatedUser, userRouter);
+app.use("/api/v1/users", authenticatedUser, userRouter);
 app.use("/api/v1/auth", authRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({
@@ -47,12 +58,16 @@ app.use("*", (req, res) => {
 const port = process.env.PORT || 5000;
 
 try {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB connected");
+
   app.listen(port, () => {
-    console.log("MongoDB connected");
     console.log(`Server started on port ${port}...`);
   });
 } catch (error) {
-  console.log("error from connecting mongoDB", error);
+  console.error("Error connecting to MongoDB:", error);
   process.exit(1);
 }
